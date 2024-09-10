688f5aad1b7d60413998906908de14b0be5f8c95
yoosufhaffejee <haffejeeyoosuf1@gmail.com>
2024-09-10 10:39:08 +0200

    Init

---
+++ src/app/components/login.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  templateUrl: './login.component.html'
+})
+export class LoginComponent implements OnInit {
+
+  ngOnInit() {
+    
+  }
+}
